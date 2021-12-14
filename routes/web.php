5784344a2803775710faf2fe5836b92395c32488
張雅茹 <3a832060@gm.student.ncut.edu.tw>
2021-12-14 09:40:27 +0800

    練習1-8:修改home路由串接HomeController

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HelloController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,8 +19,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('home',function(){
-    return view('home.index');
-});
-
+//Route::get('home',function(){
+//    return view('home.index');
+//});
+Route::get('home',[HomeController::class,'index']);
 Route::get('hello/{name?}',[HelloController::class,'index'])->name('hello.index');
